57bdb301b0a10dd7268b193c837d3df26e316b06
erickgonzalez01 <erickjoelg@gmail.com>
2025-03-11 20:55:13 -0600

    11-03-2025 Revicion

--- src/stores/url_base.js
+++ src/stores/url_base.js
@@ -1,6 +1,6 @@
 import {defineStore} from 'pinia'
 
 export const UseURLBase=defineStore("url_base",()=>{
-    const url="https://notasrapidasonline-api.erickgonzalez.net"
+    const url="http://localhost:8000"
     return {url}
 })
